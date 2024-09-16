f58c27fe24858ac3500a7abaf1b17c3239418f2e
Bloxxy213 <jijaprogamer@gmail.com>
2024-09-16 17:10:55 +0300

    .

---
+++ index.lua
@@ -0,0 +1,194 @@
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local PlayerGui = LocalPlayer.PlayerGui
+
+local Arenas = workspace.ArenasREAL
+
+local LobbyMain = PlayerGui.Lobby_Main
+local PlayButton = LobbyMain["Bottom Middle"].Start
+local RoomsParentFrame = PlayerGui.ViewRooms["Middle Middle"].ViewRooms
+local RoomsFrame = RoomsParentFrame.Background.PlayerList.Objects
+
+
+function FindLocalArena()
+    local ArenasList = Arenas:GetChildren()
+
+    for _, Arena in ipairs(ArenasList) do
+        local AreaTemplate = Arena:FindFirstChild("ArenaTemplate")
+
+        if AreaTemplate then
+            if AreaTemplate.Red.Character then
+                local Red  = AreaTemplate.Red.Character.Nametag.Frame.Nickname.Text
+                local Blue = AreaTemplate.Blue.Character.Nametag.Frame.Nickname.Text
+
+                if Red == LocalPlayer.Name or Blue == LocalPlayer.Name then
+                    return AreaTemplate.Important
+                end
+            end
+        end
+    end
+end
+
+local TicTacToe = {}
+
+function TicTacToe.checkWin(board, player)
+    local wins = {
+        {1, 2, 3}, {4, 5, 6}, {7, 8, 9},
+        {1, 4, 7}, {2, 5, 8}, {3, 6, 9},
+        {1, 5, 9}, {3, 5, 7}
+    }
+    
+    for _, win in ipairs(wins) do
+        if board[win[1]] == player and board[win[2]] == player and board[win[3]] == "_" then
+            return win[3]
+        elseif board[win[1]] == player and board[win[3]] == player and board[win[2]] == "_" then
+            return win[2]
+        elseif board[win[2]] == player and board[win[3]] == player and board[win[1]] == "_" then
+            return win[1]
+        end
+    end
+
+    return nil
+end
+
+function TicTacToe.bestMove(board)
+    local winMove = TicTacToe.checkWin(board, "X")
+    if winMove then
+        return winMove
+    end
+
+    local blockMove = TicTacToe.checkWin(board, "0")
+    if blockMove then
+        return blockMove
+    end
+
+    local strategicMoves = {5, 1, 3, 7, 9, 2, 4, 6, 8}
+
+    for _, move in ipairs(strategicMoves) do
+        if board[move] == "_" then
+            return move
+        end
+    end
+
+    return nil 
+end
+
+function getIndexFromName(name)
+    local row = tonumber(string.sub(name, 1, 1))
+    local col = 4 - tonumber(string.sub(name, 2, 2))
+    return (row - 1) * 3 + col
+end
+
+function TicTacToe.uiToBoard(name)
+    local board =  {"_", "_", "_", "_", "_", "_", "_", "_", "_"}
+    local boardUI
+
+    if name == "Tic Tac Toe" then
+        boardUI = PlayerGui.TicTacToe
+    else 
+        boardUI = PlayerGui.RushTicTacToe
+    end
+
+    local teamColor = boardUI["Top Middle Template"].RoundInfo
+    if teamColor.TeamColorRed.Visible then
+        teamColor = "O"
+    else
+        teamColor = "X"
+    end
+
+    local ArenaWorkspace = FindLocalArena()
+    local BoardBlocks = ArenaWorkspace.Drops:GetChildren()
+
+    for _, obj in ipairs(BoardBlocks) do
+        local name = obj.Name
+        local color = obj.BrickColor
+
+        local symbol
+        if color == BrickColor["Steel Blue"] then
+            symbol = "X"
+        elseif color == BrickColor.Persimmon then
+            symbol = "O"
+        end
+
+        local index = getIndexFromName(name)
+        board[index] = symbol
+    end
+
+    print(board)
+
+    return { ["board"] = board, ["teamColor"] = teamColor } 
+end
+
+
+
+function GetRooms()
+    local Rooms = {}
+
+    local RoomsFrames = RoomsFrame:GetChildren()
+    for _, RoomFrame in ipairs(RoomsFrames) do
+        if RoomFrame:IsA("Frame") then
+            local DataFrame = RoomFrame.Inside
+
+            if DataFrame:FindFirstChild("Spectate") then
+                continue
+            end
+
+            local GameName = DataFrame.GameName.Text
+            local PlayerName = string.sub(DataFrame.DisplayName.Text, 11)
+            local Robux = tonumber(DataFrame.Join.Amount.Text)
+            local JoinButton = DataFrame.Join
+
+            table.insert(Rooms, {
+                ["GameName"] = GameName,
+                ["PlayerName"] = PlayerName,
+                ["Robux"] = Robux,
+                ["JoinButton"] = JoinButton
+            })
+        end
+    end
+
+    return Rooms
+end
+
+function PressButton(button)
+    for _, connection in pairs(getconnections(button.MouseButton1Click)) do
+        connection:Fire()
+    end
+end
+
+function PressRoomsButton()
+    if not RoomsParentFrame.Visible then
+        PressButton(PlayButton)
+    end
+end
+
+function IsRoomGood(Room)
+    if Room.Robux == 0 then
+        if Room.GameName == "Rush Tic Tac Toe" or
+            Room.GameName == "Tic Tac Toe"
+        then
+            return true
+        end
+    end
+
+    return false
+end
+
+function SearchForRoom()
+    PressRoomsButton()
+
+    task.wait(5)
+
+    local Rooms = GetRooms()
+    
+    for _, Room in ipairs(Rooms) do
+        if IsRoomGood(Room) then
+            PressButton(Room.JoinButton)
+            return Room.GameName
+        end
+    end
+
+    return false
+end
+
+--local FoundRoom = SearchForRoom()
+TicTacToe.uiToBoard("Tic Tac Toe")
