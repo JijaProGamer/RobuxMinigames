fd2e12d8cd3af5e6d41c61fe69d6721d1b86b42c
Bloxxy213 <jijaprogamer@gmail.com>
2024-09-20 21:15:04 +0300

    ga

--- index.lua
+++ index.lua
@@ -382,7 +382,7 @@ function MakeGame()
 
     local AvailableProduct = PossibleAvailableGamepasses[math.random(1, #PossibleAvailableGamepasses)]
     
-    warn(AvailableProduct)
+    print(AvailableProduct)
     if RobuxChosen == 0 then
         CreateRoomsRemote:InvokeServer(
             ModeChosen, 
